55927f4cd7ab15c89afe4f4d3fc36636471355ea
stefaan <stefaan@ponnet.com>
2021-12-19 17:03:51 +0100

    UI improvements, avatar changed

--- build/wizard/src/pages/home/components/Dashboard.js
+++ build/wizard/src/pages/home/components/Dashboard.js
@@ -4,28 +4,52 @@ import API from "../../../util/API";
 import CTE from "react-click-to-edit";
 import update from 'immutability-helper';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
-import downloadandroid from "../../../assets/download-android.svg";
-import downloadios from "../../../assets/download-ios.svg";
-import downloadmac from "../../../assets/download-mac.svg";
-import downloadwin from "../../../assets/download-win.svg";
+// import downloadandroid from "../../../assets/download-android.svg";
+// import downloadios from "../../../assets/download-ios.svg";
+// import downloadmac from "../../../assets/download-mac.svg";
+// import downloadwin from "../../../assets/download-win.svg";
 import ReactTooltip from 'react-tooltip';
+import { faCopy, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./Dashboard.css";
 
-const baseurl = "https://download.zerotier.com/RELEASES/1.6.6/dist";
+// const baseurl = "https://download.zerotier.com/RELEASES/1.6.6/dist";
+
+const ClickToCopy = ({ children, text }) => {
+    const [copied, setCopied] = React.useState(false);
+    return (
+
+        <span className="icon-text">
+            <span>{children}</span>&nbsp;
+            <CopyToClipboard text={text}
+                className="icon"
+                onCopy={() => {
+                    setCopied(true)
+                    setTimeout(() => { setCopied(false) }, 2000);
+                }}>
+                <FontAwesomeIcon
+                    icon={faCopy}
+                />
+            </CopyToClipboard>
+            {copied && (<span className="is-size-6">&nbsp;Copied!</span>)}
+        </span>
+    )
+}
 
 const Comp = () => {
 
     const [showSpinner, setShowSpinner] = React.useState(true);
     const [network, setNetwork] = React.useState();
     const [networkMembers, setNetworkMembers] = React.useState();
-    const [copied, setCopied] = React.useState(false);
+    // const [copied, setCopied] = React.useState(false);
 
-    React.useEffect(() => {
-        setTimeout(() => {
-            setCopied(false);
-            ReactTooltip.rebuild()
-        }, 4000);
-        ReactTooltip.rebuild()
-    }, [copied]);
+    // React.useEffect(() => {
+    //     setTimeout(() => {
+    //         setCopied(false);
+    //         ReactTooltip.rebuild()
+    //     }, 4000);
+    //     ReactTooltip.rebuild()
+    // }, [copied]);
 
     const fetchMembers = async () => {
         const m = await API.networkMembers();
@@ -102,20 +126,32 @@ const Comp = () => {
 
             return (
                 <tr key={member.address}>
-                    <td width="5%"><input type="checkbox" checked={member.authorized} onClick={() => { setAuth(member.address, !member.authorized) }} /></td>
-                    <td>{member.address}</td>
+                    <td width="5%"><input type="checkbox" checked={member.authorized} onChange={() => { setAuth(member.address, !member.authorized) }} /></td>
+                    <td data-tip="click to edit">{member.address}</td>
                     <td>{member.authorized && member.ipAssignments && member.ipAssignments.length > 0 ? (<>yes</>) : (<>no</>)}</td>
                     <td>
+
                         <CTE
+
                             inputClassName="has-text-white"
                             initialValue={member.name}
                             endEditing={async (value) => {
-                                await API.setName(member.address, { value });
+                                await API.setNodeName(member.address, { value });
                                 fetchMembers();
                             }}
                         />
+
                     </td>
-                    {/* <td><pre>{JSON.stringify(member, 0, 2)}</pre></td> */}
+                    <td>
+                        <button
+                            disabled={member.authorized}
+                            className="button is-small"
+                            onClick={() => {
+                                API.networkDelete(member.address)
+                            }}><span>Delete</span><span className="icon is-small"><FontAwesomeIcon
+                                icon={faTrash}
+                            /></span></button>
+                    </td>
                 </tr>
             );
         }) : (
@@ -124,6 +160,7 @@ const Comp = () => {
 
         return (
             <div className="card-table">
+
                 <div className="content">
                     <table className="table">
                         <thead>
@@ -132,6 +169,7 @@ const Comp = () => {
                                 <th>Node ID</th>
                                 <th>Connected</th>
                                 <th>Description</th>
+                                <th>Commands</th>
                                 {/* <th>Qualification or relegation</th> */}
                             </tr>
                         </thead>
@@ -154,21 +192,38 @@ const Comp = () => {
                         <h1 className="title is-1 is-spaced has-text-white">AVADO Remote connection</h1>
                     </div>
                 </div>
+                <p>Here you can configure your computer, tablet or phone to connect remotely to your AVADO - please read the <a target="_blank" rel="noopener noreferrer" href="https://wiki.ava.do/tutorials/remote-connect">instructions</a> first !</p>
+                <br />
 
                 <section className="is-medium has-text-white">
                     <div className="set_setting">
-                        <h3 className="is-size-3 has-text-white">My AVADO network ID</h3>
-                        <h3 className="is-size-5 has-text-white">
-                            <CopyToClipboard key={network && network.networkid} text={network && network.networkid}
-                                onCopy={() => { setCopied(true) }}>
-                                {copied ?
-                                    (
-                                        <span className="has-text-white">{network && network.networkid}</span>
-                                    ) : (
-                                        <span data-tip={"click to copy"} className="has-text-white">{network && network.networkid}</span>
-                                    )}
-                            </CopyToClipboard>
-                        </h3>
+                        <h3 className="is-size-3 has-text-white">My AVADO network</h3>
+
+
+                        <table className="table no-lines">
+                            <tbody className="is-size-5 has-text-white">
+                                <tr>
+                                    <td>ID</td>
+                                    <td>
+                                        <ClickToCopy text={network && network.networkid}>{network && network.networkid}</ClickToCopy>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Name</td>
+                                    <td>
+                                        <CTE
+                                            inputClassName="input"
+                                            initialValue={network.network.name}
+                                            endEditing={async (value) => {
+                                                API.setNetworkName(value);
+                                                console.log(`new name ${value}`)
+                                            }}
+                                        />
+                                    </td>
+                                </tr>
+
+                            </tbody>
+                        </table>
                     </div>
                 </section>
                 <section className="is-medium has-text-white">
@@ -178,36 +233,40 @@ const Comp = () => {
                     </div>
                 </section>
                 <br />
-
+                <div className="notification is-link">
+                    <p>If you're looking for the old VPN connection page - please go <a target="_top" href="http://my.avado/#/devices">here</a>.<br/>Please note that the VPN package will be removed in future versions</p>
+                </div>
+                {/* 
                 <h1 className="title is-1 is-spaced has-text-white">Setup instructions</h1>
 
                 <h3 className="title is-4 is-spaced has-text-white">On your laptop/phone</h3>
                 <ol>
-                    <li>1. Download and install the ZeroTier client for your device</li>
+                    <li>1. Download and install the Remote Connect client for your device</li>
                     <li>
-                        <a href={`${baseurl}/ZeroTier%20One.msi`} target="_blank" title="Windows">
+
+                        <a href={`${baseurl}/ZeroTier%20One.msi`} target="_blank" rel="noopener noreferrer" title="Windows">
                             <span className="icon is-large">
-                                <img src={downloadwin} />
+                                <img alt="Download" src={downloadwin} />
                             </span>
                         </a>
-                        <a href={`${baseurl}/ZeroTier%20One.pkg`} target="_blank" title="Mac">
+                        <a href={`${baseurl}/ZeroTier%20One.pkg`} target="_blank" rel="noopener noreferrer" title="Mac">
                             <span className="icon is-large">
-                                <img src={downloadmac} />
+                                <img alt="Download" src={downloadmac} />
                             </span>
                         </a>
-                        <a href="https://play.google.com/store/apps/details?id=com.zerotier.one" target="_blank" title="Android">
+                        <a href="https://play.google.com/store/apps/details?id=com.zerotier.one" target="_blank" rel="noopener noreferrer" title="Android">
                             <span className="icon is-large">
-                                <img src={downloadandroid} />
+                                <img alt="Download" src={downloadandroid} />
                             </span>
                         </a>
-                        <a href="https://itunes.apple.com/us/app/zerotier-one/id1084101492?mt=8" target="_blank" title="iOS">
+                        <a href="https://itunes.apple.com/us/app/zerotier-one/id1084101492?mt=8" target="_blank" rel="noopener noreferrer" title="iOS">
                             <span className="icon is-large">
-                                <img src={downloadios} />
+                                <img alt="Download" src={downloadios} />
                             </span>
                         </a>
                         <br />
                     </li>
-                    <li>2. From the ZeroTier client - join the network <b>{network.networkid}</b> and check these checkboxes <b>Allow managed</b>, <b>"Allow DNS"</b> and <b>"Allow Global"</b></li>
+                    <li>2. From the client - join the network <b>{network.networkid}</b> and check these checkboxes <b>Allow managed</b>, <b>"Allow DNS"</b> and <b>"Allow Global"</b></li>
                 </ol>
                 <br /><br />
                 <h3 className="title is-4 is-spaced has-text-white">On your AVADO</h3>
@@ -221,7 +280,7 @@ const Comp = () => {
                     <li>5. disconnect from the AVADO WiFi and connect back to your home WiFi</li>
                     <li>6. wait until the client's status says <b>"OK"</b> (it might say "REQUESTING_CONFIGURATION" for a minute or so - just wait until this disappears)</li>
                     <li>7. now try connecting to <a href="http://my.ava.do">http://my.ava.do</a> <b>"OK"</b></li>
-                </ol>
+                </ol> */}
 
             </section>
             <br /><br />
